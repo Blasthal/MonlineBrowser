f24655e478a177be66a12eb7eaad7ffcb9fef7b8
Kaku Tatsunori <gameprogramer@live.jp>
2016-01-19 22:51:56 +0900

    バッチファイル内の不要なコメントを削除。

--- Publish_Release.bat
+++ Publish_Release.bat
@@ -5,13 +5,12 @@ echo exeにdllをマージします。
 echo.
 
 rem ILMerge.exeへのパス
-rem set ILMERGE_PATH="S:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe"
 set ILMERGE_PATH=ILMerge\ILMerge.exe
 rem Merge対象のディレクトリとファイル
 set WORK_DIR=MonlineBrowser\bin\Release\
 set INPUT_FILE=MonlineBrowser.exe
 
-:: 作業用のtempディレクトリを作成する
+rem 作業用のtempディレクトリを作成する
 set TEMP_DIR=__TEMP__\
 if NOT EXIST %TEMP_DIR% (
 mkdir %TEMP_DIR%
@@ -34,7 +33,7 @@ mkdir %PUBLISH_DIR%
 )
 move %OUTPUT_DIR%%INPUT_FILE% %PUBLISH_DIR%
 
-:: tempディレクトリを削除する
+rem tempディレクトリを削除する
 if EXIST %TEMP_DIR% (
 rmdir /s /q %TEMP_DIR%
 )
